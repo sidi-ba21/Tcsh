08e3e54f64da52187befd4fa068cff6dac9e23c1
Sidi BA <sasuke_uchiwa@localhost.localdomain>
2021-10-04 11:09:02 +0200

    [ADD] globbing and inhibitors directory

---
+++ src/globbing/globbing.c
@@ -0,0 +1,10 @@
+/*
+** EPITECH PROJECT, 2021
+** 42sh
+** File description:
+** globbing
+*/
+
+#include "minishell.h"
+#include "my.h"
+
---
+++ src/inhibitors/inhibitors.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2021
+** 42sh
+** File description:
+** inhibitors
+*/
+
+#include "minishell.h"
+#include "my.h"
+
+char **append_str(char)
